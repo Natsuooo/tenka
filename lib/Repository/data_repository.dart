81a72184d811e78651309e019bd479fd570720cf
Natsuooo <n17975775@gmail.com>
2020-10-26 14:42:55 +0900

    キャッシュ削除

--- lib/Repository/data_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:tenka_2_0_0/Entity/data.dart';
-import 'package:tenka_2_0_0/Dao/data_dao.dart';
-
-class DataRepository {
-  final dataDao = DataDao();
-
-  Future getAllData() => dataDao.getAll();
-}
--- lib/Repository/star_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:tenka_2_0_0/Dao/star_dao.dart';
-
-class StarRepository {
-  final starDao = StarDao();
-
-  Future getStarList() => starDao.getStarList();
-
-  Future saveStarList(List<String> starList) => starDao.saveStarList(starList);
-}
